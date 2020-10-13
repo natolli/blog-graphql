0f31712e90cf261139c5f1874673dc4549f7300c
Natoli Lemma <natolilemma1@gmail.com>
2020-10-13 04:07:03 -0400

    (feat) login with redis cache

--- src/index.ts
+++ src/index.ts
@@ -4,19 +4,52 @@ import Express from 'express'
 import {buildSchema} from 'type-graphql'
 import { createConnection } from "typeorm";
 import { RegisterResolver } from "./modules/user/Register";
-
+import {LoginResolver} from './modules/user/Login'
+import { MeResolver } from "./modules/user/Me";
+import session from 'express-session'
+import {redis} from './redis'
+import connectRedis from 'connect-redis'
+import cors from 'cors'
 
 const main =async () =>{
 
     await createConnection();
 
     const schema = await buildSchema({
-        resolvers:[RegisterResolver]
+        resolvers:[MeResolver,LoginResolver,RegisterResolver]
     })
 
-    const apolloServer = new ApolloServer({schema})
+    const apolloServer = new ApolloServer({schema,
+        context:({req}: any)=> ({req})
+    })
 
     const app = Express();
+
+    const RedisStore = connectRedis(session);
+
+    app.use(
+        cors({
+            credentials:true,
+            origin: "http://localhost:3000"
+        })
+    )
+
+    app.use(
+        session({
+            store:new RedisStore({
+                client: redis as any
+            }), 
+            name: 'qid',
+            secret: 'ofgasdfigsf',
+            resave:false,
+            saveUninitialized: false,
+            cookie:{
+                httpOnly: true,
+                secure: process.env.NODE_ENV === "production",
+                maxAge: 1000 * 60 * 60 * 24 * 7 * 365 // 7 years
+            }
+        })
+    )
 
     apolloServer.applyMiddleware({app})
 
---
+++ src/modules/user/Login.ts
@@ -0,0 +1,34 @@
+import {Arg, Ctx, Mutation, Resolver} from 'type-graphql'
+import { User } from  '../../entity/User';
+import bcrypt from 'bcryptjs';
+import {LoginInput} from './login/LoginInput'
+import {MyContext} from '../../types/MyContext'
+
+@Resolver()
+export class LoginResolver{
+
+    @Mutation(()=> User,{nullable:true})
+    async login(
+        @Arg('input') {email,password}:LoginInput,
+        @Ctx() ctx:MyContext   
+     ): Promise<User | null>{
+            
+        const user = await User.findOne({where:{email}})
+
+        if(!user){
+            return null
+        }
+
+        const valid = bcrypt.compare(password, user.password)
+
+        
+        if(!valid){
+            return null
+        }
+
+        ctx.req.session!.userId =  user.id
+        
+
+        return user;
+    }
+}
